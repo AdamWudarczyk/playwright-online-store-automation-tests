088f80fd0d7b1b3a4eff136621205b957a97d38e
Adam <wudarczykadam@gmail.com>
2025-11-28 22:41:56 +0100

    Add InventoryPage POM with basic product and cart interactions

---
+++ pages/InventoryPage.js
@@ -0,0 +1,28 @@
+// pages/InventoryPage.js
+import { expect } from '@playwright/test';
+
+/** @typedef {import('@playwright/test').Page} Page */
+
+export class InventoryPage {
+    /**
+     * @param {Page} page
+     */
+    constructor(page) {
+        this.page = page;
+        this.inventoryList = page.locator('.inventory_list');
+        this.cartLink = page.locator('.shopping_cart_link');
+    }
+
+    async addProductByIndex(index) {
+        const addButtons = this.page.locator('.inventory_item button');
+        await addButtons.nth(index).click();
+    }
+
+    async goToCart() {
+        await this.cartLink.click();
+    }
+
+    async assertOnInventoryPage() {
+        await expect(this.inventoryList).toBeVisible();
+    }
+}
