2baafc04c1d01921180aa28c15a4c4346240ae12
Adam <wudarczykadam@gmail.com>
2025-11-28 22:51:20 +0100

    Add CheckoutPage POM with information form and order finish methods

---
+++ pages/CheckoutPage.js
@@ -0,0 +1,29 @@
+import { expect } from '@playwright/test';
+
+export class CheckoutPage {
+    constructor(page) {
+        this.page = page;
+
+        this.firstName = page.locator('[data-test="firstName"]');
+        this.lastName = page.locator('[data-test="lastName"]');
+        this.postalCode = page.locator('[data-test="postalCode"]');
+        this.continueBtn = page.locator('[data-test="continue"]');
+        this.finishBtn = page.locator('[data-test="finish"]');
+        this.thankYouMessage = page.locator('.complete-header');
+    }
+
+    async fillInformation(first, last, postal) {
+        await this.firstName.fill(first);
+        await this.lastName.fill(last);
+        await this.postalCode.fill(postal);
+        await this.continueBtn.click();
+    }
+
+    async finishOrder() {
+        await this.finishBtn.click();
+    }
+
+    async assertOrderFinished() {
+        await expect(this.thankYouMessage).toHaveText('Thank you for your order!');
+    }
+}
