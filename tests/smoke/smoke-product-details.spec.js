aa842ece1d13ee43a553d8502b704ab89837ad13
Adam <wudarczykadam@gmail.com>
2025-12-05 21:57:31 +0100

    add new smoke tests SMK-07 and SMK-08

--- tests/smoke/smoke-product-details.spec.js
+++ tests/smoke/smoke-product-details.spec.js
@@ -3,10 +3,11 @@ import {LoginPage} from "../../pages/LoginPage";
 import {InventoryPage} from "../../pages/InventoryPage";
 import users from '../../fixtures/users.json' with { type: 'json' };
 
-let loginPage;
-let inventoryPage;
-
 test.describe('Smoke - Inventory', () => {
+
+    let loginPage;
+    let inventoryPage;
+
     test.beforeEach(async ({page}) => {
         loginPage = new LoginPage(page);
         inventoryPage = new InventoryPage(page);
@@ -23,5 +24,26 @@ test.describe('Smoke - Inventory', () => {
         await firstProduct.click();
         const detailsTitle = page.locator('.inventory_details_name');
         await expect(detailsTitle).toHaveText(productTitle ?? '');
+    });
+
+    test('SMK-07: sorting Z→A works @smoke', async ({ page }) => {
+        await loginPage.login(users.validUser.username, users.validUser.password);
+        await inventoryPage.assertOnInventoryPage();
+
+        await page.selectOption('.product_sort_container', 'za');
+
+        const names = await page.locator('.inventory_item_name').allTextContents();
+        const sorted = [...names].sort().reverse();
+        expect(names).toEqual(sorted);
+    });
+
+    test('SMK-08: adding item shows cart badge @smoke', async ({ page }) => {
+        await loginPage.login(users.validUser.username, users.validUser.password);
+        await inventoryPage.assertOnInventoryPage();
+
+        await page.locator('button.btn_inventory').first().click();
+        const badge = page.locator('.shopping_cart_badge');
+
+        await expect(badge).toHaveText('1');
     });
 });
