7db65a6dc32f8e11f034df72254e28d06ed3c547
Adam <wudarczykadam@gmail.com>
2025-12-07 19:54:10 +0100

    add new product browsing e2e tests

---
+++ tests/e2e/e2e-product-browsing.spec.js
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../../pages/LoginPage.js';
+import { InventoryPage } from '../../pages/InventoryPage.js';
+import users from '../../fixtures/users.json' with { type: 'json' };
+
+test.describe('E2E - Product Browsing', () => {
+
+    let loginPage;
+    let inventoryPage;
+
+    test.beforeEach(async ({ page }) => {
+        loginPage = new LoginPage(page);
+        inventoryPage = new InventoryPage(page);
+
+        await loginPage.goto();
+        await loginPage.login(users.validUser.username, users.validUser.password);
+        await inventoryPage.assertOnInventoryPage();
+    });
+
+    test('E2E-05: user can open product details and return to inventory @e2e', async ({ page }) => {
+        const firstProduct = page.locator('.inventory_item_name').first();
+        const productName = await firstProduct.textContent();
+
+        await firstProduct.click();
+        await expect(page.locator('.inventory_details_name')).toHaveText(productName ?? '');
+
+        await page.locator('#back-to-products').click();
+        await inventoryPage.assertOnInventoryPage();
+    });
+
+});
